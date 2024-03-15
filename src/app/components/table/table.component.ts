4d50f6dffe75c024e83bffdeb9f0a09d2cc67b1d
Uriel Dávila <uri.davi.14@gmail.com>
2024-03-15 14:24:04 -0600

    Fix - Bug on Sort Table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,11 @@
 import { Country } from './../../models/country.interface';
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { CountriesService } from '../../services/countries.service';
@@ -12,9 +18,16 @@ import { Subscription } from 'rxjs';
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
-export class TableComponent implements OnInit, OnDestroy {
+export class TableComponent implements OnInit, OnDestroy, AfterViewInit {
   regionSelected: string = '';
-  displayedColumns: string[] = ['flag', 'name', 'population', 'area', 'region', 'detail'];
+  displayedColumns: string[] = [
+    'flag',
+    'name',
+    'population',
+    'area',
+    'region',
+    'detail',
+  ];
   dataSource: MatTableDataSource<Country> = new MatTableDataSource<Country>([]);
   private subscriptions: Subscription[] = [];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -23,40 +36,53 @@ export class TableComponent implements OnInit, OnDestroy {
     private countriesService: CountriesService,
     private filterService: FilterService
   ) {}
+  ngAfterViewInit(): void {}
   ngOnDestroy(): void {
     this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
   ngOnInit(): void {
     this.getCountries();
     this.applyFilter();
-    this.onSort();
+    this.checkSort();
   }
   getCountries() {
-    this.subscriptions.push(this.countriesService.getCountries().subscribe((res: Country[]) => {
-      this.dataSource = new MatTableDataSource(res);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-    }));
+    this.subscriptions.push(
+      this.countriesService.getCountries().subscribe((res: Country[]) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.sortTable('population');
+      })
+    );
   }
   applyFilter() {
-    this.subscriptions.push(this.filterService.getFilterData().subscribe((region) => {
-      this.regionSelected = region;
-      this.dataSource.filter = this.regionSelected.trim().toLowerCase();
-      if (this.dataSource.paginator) {
-        this.dataSource.paginator.firstPage();
-      }
-    }));
+    this.subscriptions.push(
+      this.filterService.getFilterData().subscribe((region) => {
+        this.regionSelected = region;
+        this.dataSource.filter = this.regionSelected.trim().toLowerCase();
+        if (this.dataSource.paginator) {
+          this.dataSource.paginator.firstPage();
+        }
+      })
+    );
   }
-  onSort() {
-    this.subscriptions.push(this.filterService.getSort().subscribe((res) => {
-      if (res) {
-        this.dataSource.sort = this.sort;
-        this.dataSource.sort.sort({
-          id: res,
-          start: 'asc',
-          disableClear: true,
-        });
-      }
-    }));
+  sortTable(name: string) {
+    if (this.dataSource.sort) {
+      this.dataSource.sort.sort({
+        id: name,
+        start: 'asc',
+        disableClear: true,
+      });
+    }
+  }
+  checkSort() {
+    let sort = '';
+    this.subscriptions.push(
+      this.filterService.getSort().subscribe((res) => {
+        if (!res) sort = 'population';
+        else sort = res;
+        this.sortTable(sort);
+      })
+    );
   }
 }
